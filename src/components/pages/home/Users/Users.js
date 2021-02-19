95c6aef2f8e5682e90e1bae7af7dff2d0e505ef1
tgafiulin <t.gafiulin@ya.ru>
2021-02-19 14:34:52 +0300

    add script for users output

--- src/components/pages/home/Users/Users.js
+++ src/components/pages/home/Users/Users.js
@@ -1,5 +1,34 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from 'app/reducers/users';
+import Loader from 'components/common/Loader/Loader'
+
 function Users () {
-    return 'users';
+    const dispatch = useDispatch();
+    const { users, loading } = useSelector(state => state.users);
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [])
+
+    return <>
+    {
+        loading 
+        ?
+            <Loader /> 
+        :
+            <table>
+                <thead>
+                    <tr><th>id</th><th>name</th><th>email</th></tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user) => <tr key={user.id}><td>{user.id}</td><td>{user.name}</td><td>{user.email}</td></tr>)
+                    }
+                </tbody>
+            </table>
+    }
+    </>;
 }
 
 export default Users;
