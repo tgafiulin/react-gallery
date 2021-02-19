0e407bbdcdfda20357b6f5ffaafc71f01337099c
tgafiulin <t.gafiulin@ya.ru>
2021-02-19 14:34:31 +0300

    add fetching of users

---
+++ src/app/reducers/users.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchUsers = createAsyncThunk(
+    'users/fetchUsers',
+    async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/')
+        return {
+            users: await response.json()
+        }
+    }
+)
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState: {
+        loading: true,
+        users: []
+    },
+    reducers: {
+
+    },
+    extraReducers: {
+        [fetchUsers.fulfilled]: (state, action) => {
+            state.users = action.payload.users;
+            state.loading = false;
+        },
+    }
+})
+
+// export const { incrementLargestID, deleteNote, updateTodoList, addNote } = notesSlice.actions
+export default usersSlice.reducer
+
--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import usersSlice from 'app/reducers/users'
 
 export default configureStore({
   reducer: {
+    users: usersSlice
   },
 });
