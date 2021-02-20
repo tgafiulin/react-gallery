5c79aac0f15a3dfa2c1b86ba95c1dbd7a796e226
tgafiulin <t.gafiulin@ya.ru>
2021-02-20 15:33:21 +0300

    add User page layout

--- src/components/pages/user/User/User.js
+++ src/components/pages/user/User/User.js
@@ -1,5 +1,31 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAlbums } from 'app/reducers/albums/albums'
+import Loader from 'components/common/Loader/Loader'
+import Album from 'components/pages/user/Album/Album'
+
 function User () {
-    return 'user';
+    const dispatch = useDispatch();
+    const {loading, albums} = useSelector(state => state.albums);
+    const { id } = useParams();
+
+    useEffect(() => {
+        dispatch(fetchAlbums(id));
+    }, [])
+
+    return <>
+        {loading 
+        ?
+            <Loader /> 
+        :
+            <div className="albums-list">
+                {
+                    albums[id].map((album) => <Album key={album.id} album={album} />)
+                }
+            </div>
+        }   
+    </>;
 }
 
 export default User;
