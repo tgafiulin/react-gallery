99711727b78bda7c30a1ac65ddc011131e1b4b81
tgafiulin <t.gafiulin@ya.ru>
2021-02-23 13:15:01 +0300

    refactoring albumlist component

--- src/components/pages/user/AlbumList/AlbumList.js
+++ src/components/pages/user/AlbumList/AlbumList.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 function AlbumList ({album}) {
     const {id, title, baseImgUrl, photosLength} = album;
 
-    return <div className="album">
+    return <div className="album-list">
         <Link to={`/album/${id}`}></Link>
         <img src={baseImgUrl} alt={title} />
-        <div className="album__content">
-            <div className="album__title">Album's name: {title}</div>
-            <div className="album__photos">Number of photos: {photosLength}</div>
+        <div className="album-list__content">
+            <div className="album-list__title">Album's name: {title}</div>
+            <div className="album-list__photos">Number of photos: {photosLength}</div>
         </div>
     </div>
 }
