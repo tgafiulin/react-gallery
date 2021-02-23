6ba2bcb52f231ea7ea1791b4e3625bf3104e2344
tgafiulin <t.gafiulin@ya.ru>
2021-02-23 13:15:18 +0300

    refactoring photo component

--- src/components/pages/user/Photo/Photo.js
+++ src/components/pages/user/Photo/Photo.js
@@ -1,6 +1,6 @@
 function Photo({id, title, thumbnailUrl, onClick}) {
     return <div className="photo" onClick={onClick}>
-        <img src={thumbnailUrl} alt={title} width="120" height="123" />
+        <img src={thumbnailUrl} alt={title} />
     </div>
 }
 
