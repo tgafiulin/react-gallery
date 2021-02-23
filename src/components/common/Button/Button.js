2133f4465f40f0d35a95ceec6aa8acfb51d52b6d
tgafiulin <t.gafiulin@ya.ru>
2021-02-23 13:14:34 +0300

    refactoring button component

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,7 +1,7 @@
 import './Button.scss'
 
 function Button({onClick, value, className}) {
-    return <button onClick={onClick} className={className}>{value ? value : ''}</button>
+    return <button onClick={onClick} className={className}>{value}</button>
 }
 
 export default Button;
